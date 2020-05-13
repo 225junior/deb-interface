7d36ef928fc887f7f8afc53d7bf5d837cea7e073
Deborah Meda <deborahmeda96@gmail.com>
2020-05-13 21:45:34 +0000

    gestion_interface

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Connexion a la base de données Echuée
-    $bd = new PDO('mysql:host=localhost;dbname=debo;charset=utf8', 'root', '');
+    $bd = new PDO('mysql:host=10.224.78.20;dbname=interface_web;charset=utf8', 'root', '');
 
 } catch (Exception $e) {
     // Connexion a la base de données Echuée
