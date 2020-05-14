13d5438d5e370363be02679832a1b6818d504d31
225junior <225juniorbrindou@gmail.com>
2020-05-14 20:41:40 +0000

    links and pages

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,22 +18,22 @@
 
 		<li class="sidebar-header">Fiches</li>
 		<li>
-			<a href="forms.php">
-         	<i class="zmdi zmdi-assignment"></i> <span>Interene de Sortie</span>
+			<a href="interne_sortie.php">
+         	<i class="zmdi zmdi-assignment"></i> <span>Interne de Sortie</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
-         	<i class="zmdi zmdi-assignment"></i> <span>Recette definitive</span>
+			<a href="recette_definitive.php">
+         	<i class="zmdi zmdi-assignment"></i> <span>Recette définitive</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="bon_livraison_partiel.php">
          	<i class="zmdi zmdi-assignment-check"></i> <span>Bon de livraison Partiel</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="bon_livraison.php">
          	<i class="zmdi zmdi-assignment-check"></i> <span>Bon de livraison</span>
         	</a>
 		</li>
@@ -42,27 +42,27 @@
 
       <li class="sidebar-header">ÉQUIPPEMENTS</li>
       <li>
-			<a href="forms.php">
+			<a href="equippements.php">
          	<i class="zmdi zmdi-assignment-o"></i> <span>Tous les équippements</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="aquippement_livre.php">
          	<i class="zmdi zmdi-assignment-o"></i> <span>Équipements Livré</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="aquippement_manquant.php">
          	<i class="zmdi zmdi-assignment-o"></i> <span>Équippements Manquants</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="historique.php">
          	<i class="zmdi zmdi-8tracks"></i> <span>Historique</span>
         	</a>
 		</li>
 		<li>
-			<a href="forms.php">
+			<a href="observation.php">
          	<i class="zmdi zmdi-album"></i> <span>Observation</span>
         	</a>
 		</li>
