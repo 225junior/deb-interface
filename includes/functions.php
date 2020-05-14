6b3cee353a104d60ee8aa6e018db39ae254a82b8
225junior <225juniorbrindou@gmail.com>
2020-05-14 14:06:22 +0000

    dashbord connection ok

--- includes/functions.php
+++ includes/functions.php
@@ -13,7 +13,6 @@ function logged_only(){
 
     if (!isset($_SESSION['auth'])) {
         $_SESSION['flash']['danger'] = "Vous n'avez pas le droit";
-        // header('Location:index.php');
         echo '<script> document.location.replace("index.php"); </script>';
         exit();
     }
--- includes/topbar.php
+++ includes/topbar.php
@@ -32,8 +32,8 @@
            <div class="media">
              <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
             <div class="media-body">
-            <h6 class="mt-2 user-title"></h6>
-            <p class="user-subtitle">mccoy@example.com</p>
+            <h6 class="mt-2 user-title"> <?= $_SESSION['auth']->name ?></h6>
+            <p class="user-subtitle"><?= $_SESSION['auth']->email ?></p>
             </div>
            </div>
           </a>
